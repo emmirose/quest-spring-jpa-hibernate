0a09ca77b384bf6673331dba20d43594646c2121
Emma Rosso <emmarosso.pro@gmail.com>
2023-06-29 13:48:42 +0200

    quest completed

--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,10 +1,19 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 // TODO : update this entity
 
+@Entity
 public class School {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private Long capacity;
     private String country;
